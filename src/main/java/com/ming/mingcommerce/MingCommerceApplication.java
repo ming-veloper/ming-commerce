4afebadfada79f51397afc508472be92baf84059
yeonnex <yeonnex@gmail.com>
2023-01-06 17:05:51 +0900

    init

---
+++ src/main/java/com/ming/mingcommerce/MingCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ming.mingcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MingCommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ming/mingcommerce/MingCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ming.mingcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingCommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
