58beebbc07cf007d9e8a60cb9bc79d1e847c1e4c
yeonnex <yeonnex@gmail.com>
2023-01-15 23:32:48 +0900

    X-WWW-MING-AUTHORIZATION 헤더값 null 처리

--- src/main/java/com/ming/mingcommerce/security/CustomAuthorizationFilter.java
+++ src/main/java/com/ming/mingcommerce/security/CustomAuthorizationFilter.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -24,10 +25,14 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String header = request.getHeader("X-WWW-MING-AUTHORIZATION");
+
+        if (StringUtils.hasText(header)) {
         CurrentUser currentUser = jwtTokenUtil.verifyToken(header.replace("Bearer ", ""));
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(currentUser, "", getAuthorities(currentUser.getRole()));
 
         SecurityContextHolder.getContext().setAuthentication(token);
+        }
+
         filterChain.doFilter(request, response);
     }
 
